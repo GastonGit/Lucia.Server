969a8cbce59b5641a32ee6054a68af5884ded75c
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-24 15:57:11 +0100

    setup inital api response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,115 @@ import { Router, Request, Response } from 'express';
 const indexRouter = Router();
 
 indexRouter.get('/', (_req: Request, res: Response) => {
-    res.send('Boiler!');
+    res.send(itemData);
 });
+
+const itemData = [
+    {
+        img: 'logo512.png',
+        title: 'Breakfast',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Burger',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Camera',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Coffee',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Hats',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Honey',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Basketball',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Fern',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Mushrooms',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Tomato basil',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Sea star',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Bike',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Coffee',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Hats',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Honey',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Basketball',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Fern',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Mushrooms',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Tomato basil',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Sea star',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Bike',
+        author: '@test',
+    },
+];
 
 export default indexRouter;
