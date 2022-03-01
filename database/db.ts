1ca4e262c1a76e93462dc26fbbd9478d1a931147
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-03-01 22:13:02 +0100

    add maxPageCount to search response

--- database/db.ts
+++ database/db.ts
@@ -3,6 +3,19 @@ import knexFile from '../knexfile';
 
 const db = knex(knexFile);
 
+interface PureManga {
+    id: string;
+    directory: string;
+    title: string;
+    author: boolean;
+    bucket: string;
+    thumbnail: string;
+    series: string;
+    universe: string;
+    chapter: number;
+    created_at: string;
+}
+
 class Database {
     async getMaxPageCount() {
         const query = await db('manga').count().first();
@@ -17,10 +30,11 @@ class Database {
     }
 
     async getGallery(page = 1) {
-        return db('manga')
+        const mangas = await db('manga')
             .select()
             .offset((page - 1) * 21)
             .limit(21);
+        return this.trimMangaResponse(mangas as PureManga[]);
     }
 
     async getManga(id: number) {
@@ -44,9 +58,47 @@ class Database {
         }
     }
 
-    async findMangaByTitle(title: string) {
+    trimMangaResponse(pureGallery: PureManga[]) {
+        const gallery = [];
+        for (let i = 0; i < pureGallery.length; i++) {
+            gallery.push({
+                id: pureGallery[i].id,
+                title: pureGallery[i].title,
+                author: pureGallery[i].author,
+                thumbnail:
+                    process.env.SERVER_URL +
+                    '/media/' +
+                    pureGallery[i].directory +
+                    '/' +
+                    pureGallery[i].thumbnail,
+            });
+        }
+        return gallery;
+    }
+
+    async findMangaByTitle(title: string, page: number) {
         const trimmedTitle = title.replace(/[^A-Za-z0-9\s!?]/g, '');
-        return db('manga').whereLike('title', '%' + trimmedTitle + '%');
+        const mangas = await db('manga')
+            .where('active', 1)
+            .whereLike('title', '%' + trimmedTitle + '%')
+            .offset((page - 1) * 21)
+            .limit(21);
+
+        const maxCount = await db('manga')
+            .whereLike('title', '%' + trimmedTitle + '%')
+            .count()
+            .first();
+
+        const maxPageCount = Math.ceil(
+            (typeof maxCount?.['count(*)'] === 'number'
+                ? maxCount['count(*)']
+                : 1) / 21,
+        );
+
+        return {
+            gallery: this.trimMangaResponse(mangas as PureManga[]),
+            maxPageCount: maxPageCount,
+        };
     }
 }
 
--- src/routes/search.ts
+++ src/routes/search.ts
@@ -4,10 +4,15 @@ import db from '../../database/db';
 
 searchRouter.get('/', async (req: Request, res: Response) => {
     const queryTitle = req.query.title;
+    const queryPage = req.query.page;
+    let page = 1;
+
+    if (typeof queryPage === 'string' && !Number.isNaN(parseInt(queryPage))) {
+        page = parseInt(queryPage);
+    }
 
     if (typeof queryTitle === 'string') {
-        const foundMangas = await db.findMangaByTitle(queryTitle);
-
+        const foundMangas = await db.findMangaByTitle(queryTitle, page);
         res.send(foundMangas);
     } else {
         res.status(400).send('Bad request');
